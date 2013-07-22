4a24bb1adbaf3c6af1d5db50b023d65b1b408a35
Jing Quanqi <jingquanqi@gmail.com>
2013-07-22 10:58:52 +0800

    simple and can use

---
+++ src/org/quanqi/passwordbuilder/Password.java
@@ -0,0 +1,43 @@
+package org.quanqi.passwordbuilder;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class Password extends Activity {
+	
+	private WebView mPassWebView;
+	final static String URL = "file:///android_asset/p.html";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_password);
+		this.initPassWebview();
+		
+	}
+	
+	private void initPassWebview() {
+		this.mPassWebView = (WebView) findViewById(R.id.webView_password_builder);
+		this.mPassWebView.setWebChromeClient(new myWebChromeClient());
+		WebSettings settings = this.mPassWebView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		
+		this.mPassWebView.loadUrl(URL);
+	}
+	
+	private class myWebChromeClient extends WebChromeClient {
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.password, menu);
+		return true;
+	}
+
+}
